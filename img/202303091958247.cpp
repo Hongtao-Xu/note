ed8a26f02b680fa3e67e36e87449958e2e56789f
Hongtao-Xu <63973487+Hongtao-Xu@users.noreply.github.com>
2023-03-09 19:58:50 +0800

    Upload by PicGo

---
+++ img/202303091958247.cpp
@@ -0,0 +1,53 @@
+#include "tcpclient.h"
+#include "ui_tcpclient.h"
+#include <QByteArray>
+#include <QDebug>
+#include <QMessageBox>
+#include <QHostAddress>
+
+TcpClient::TcpClient(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::TcpClient)
+{
+    ui->setupUi(this);
+    loadConfig();
+
+    connect(&m_tcpSocket, SIGNAL(connected())
+            , this, SLOT(showConnect()));
+
+    //连接服务器
+    m_tcpSocket.connectToHost(QHostAddress(m_strIP), m_usPort);
+}
+
+TcpClient::~TcpClient()
+{
+    delete ui;
+}
+
+void TcpClient::loadConfig()
+{
+    QFile file(":/client.config");
+    if (file.open(QIODevice::ReadOnly))
+    {
+        QByteArray baData = file.readAll();
+        QString strData = baData.toStdString().c_str();
+        file.close();
+
+        strData.replace("\r\n", " ");
+
+        QStringList strList = strData.split(" ");
+
+        m_strIP = strList.at(0);
+        m_usPort = strList.at(1).toUShort();
+        qDebug() << "ip:" << m_strIP << " port:" << m_usPort;
+    }
+    else
+    {
+        QMessageBox::critical(this, "open config", "open config failed");
+    }
+}
+
+void TcpClient::showConnect()
+{
+    QMessageBox::information(this, "连接服务器", "连接服务器成功");
+}
